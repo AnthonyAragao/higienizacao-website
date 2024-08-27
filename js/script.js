7060d9951ab3ebf5f644347ec644ab4cb002c740
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2024-08-27 09:40:54 -0300

    style: ajustes na transição para abrir a descrição

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 function toggleDescription(container) {
     const question = container.closest('.faq__question');
-    
-    if (question.classList.contains('active')) {
-        question.classList.remove('active');
-    } else {
-        question.classList.add('active');
-    }
+    question.classList.toggle('active');
 }
