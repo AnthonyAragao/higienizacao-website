04487431f9169d76596333c4cfe1d06b20a47891
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2024-08-30 13:57:17 -0300

    style: Update mobile menu and header layout

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ function toggleDescription(container) {
     const question = container.closest('.faq__question');
     question.classList.toggle('active');
 }
+
+function toggleMobileMenu() {
+    const menu = document.querySelector('.header__mobile-menu');
+    const body = document.querySelector('body');
+    menu.classList.toggle('active');
+    body.classList.toggle('overflow-hidden');
+}
